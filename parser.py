d0645b196f8a6dc307b933fc039238825f77c032
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-12-30 22:24:16 -0800

    output number of people in the country

--- parser.py
+++ parser.py
@@ -5,23 +5,35 @@ import csv
 	# print 'My IP:', match
 csv_data = []
 unknown_data = []
+country_dict = {}
 input_file = csv.DictReader(open("subs.csv"))
 for row in input_file:
 	csv_data.append(row)
 
 with open_database('GeoLite2-City.mmdb') as db:
 	for a in csv_data:
-		if not a["CC"]:
+		if a["CC"]:
+			cc = a["CC"]
+			if cc in country_dict:
+				country_dict[cc] +=1
+			else:
+				country_dict[cc] = 1
+		else:
 		# print a["Email Address"]
 			c = a["CONFIRM_IP"]
 			# print type(c)
 			match = geolite2.lookup(c)
 			# print type(a["CONFIRM_IP"])
 			if match:
-				print match.country
+				# print match.country
+				a["CC"] = match.country
+				if match.country in country_dict:
+					country_dict[match.country] +=1
 			else:
 				unknown_data.append(a)
 
-for u in unknown_data:
-	print u["Email Address"]
+print country_dict
+
+# for u in unknown_data:
+# 	print u["Email Address"]
 			
