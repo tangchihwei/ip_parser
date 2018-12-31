870c80cb50a633b33bbba901fdce5d81d264e212
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-12-30 21:50:08 -0800

    .

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
 from geoip import open_database, geolite2
+import csv
+# with open_database('GeoLite2-City.mmdb') as db:
+# 	# match = db.lookup_mine()
+# 	match = geolite2.lookup('69.244.37.190')
+# 	print 'My IP:', match
+data = []
+input_file = csv.DictReader(open("cleaned.csv"))
+for row in input_file:
+	data.append(row)
 
-with open_database('GeoLite2-City.mmdb') as db:
-	# match = db.lookup_mine()
-	match = geolite2.lookup('69.244.37.190')
-	print 'My IP:', match
-
+print data[1]	
