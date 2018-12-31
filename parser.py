82a8068c486c6afbf1f7af4a4fa00eacd38e0e03
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-12-30 22:13:30 -0800

    working

--- parser.py
+++ parser.py
@@ -1,12 +1,27 @@
 from geoip import open_database, geolite2
 import csv
 # with open_database('GeoLite2-City.mmdb') as db:
-# 	# match = db.lookup_mine()
 # 	match = geolite2.lookup('69.244.37.190')
-# 	print 'My IP:', match
-data = []
-input_file = csv.DictReader(open("cleaned.csv"))
+	# print 'My IP:', match
+csv_data = []
+unknown_data = []
+input_file = csv.DictReader(open("subs.csv"))
 for row in input_file:
-	data.append(row)
+	csv_data.append(row)
 
-print data[1]	
+with open_database('GeoLite2-City.mmdb') as db:
+	for a in csv_data:
+		if not a["CC"]:
+		# print a["Email Address"]
+			c = a["CONFIRM_IP"]
+			# print type(c)
+			match = geolite2.lookup(c)
+			# print type(a["CONFIRM_IP"])
+			if match:
+				print match.country
+			else:
+				unknown_data.append(a)
+
+for u in unknown_data:
+	print u["Email Address"]
+			
