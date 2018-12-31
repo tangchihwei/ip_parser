d190541aa577b78403e4a925ef3b591bcca0c4cc
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-12-30 23:06:00 -0800

    working

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 from geoip import open_database, geolite2
+import pycountry
+from pycountry import countries
 import csv
-# with open_database('GeoLite2-City.mmdb') as db:
-# 	match = geolite2.lookup('69.244.37.190')
-	# print 'My IP:', match
+
 csv_data = []
 unknown_data = []
 country_dict = {}
@@ -14,26 +14,28 @@ with open_database('GeoLite2-City.mmdb') as db:
 	for a in csv_data:
 		if a["CC"]:
 			cc = a["CC"]
+
 			if cc in country_dict:
 				country_dict[cc] +=1
 			else:
 				country_dict[cc] = 1
 		else:
-		# print a["Email Address"]
 			c = a["CONFIRM_IP"]
-			# print type(c)
 			match = geolite2.lookup(c)
-			# print type(a["CONFIRM_IP"])
 			if match:
-				# print match.country
 				a["CC"] = match.country
 				if match.country in country_dict:
 					country_dict[match.country] +=1
 			else:
 				unknown_data.append(a)
 
-print country_dict
+for c in country_dict:
+	if c == "UK":
+		country_dict['GB'] = country_dict.pop(c)
+		c = 'GB'
+	else:
+		print pycountry.countries.get(alpha_2=c).name + " : " + str(country_dict[c])
 
-for u in unknown_data:
-	print u["Email Address"] + ": " + u["CONFIRM_IP"]
+# for u in unknown_data:
+# 	print u["Email Address"] + ": " + u["CONFIRM_IP"]
 			
