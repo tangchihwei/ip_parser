3745733e00df522c5abea798fdffc385eb491630
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2019-01-01 16:34:04 -0800

    region first commit

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ import csv
 csv_data = []
 unknown_data = []
 country_dict = {}
+region_dict = {}
 input_file = csv.DictReader(open("subs.csv"))
 for row in input_file:
 	csv_data.append(row)
@@ -36,6 +37,29 @@ country_dict['GB'] = country_dict.pop('UK',None)
 for c in country_dict:
 	if c:
 		print pycountry.countries.get(alpha_2=c).name + " : " + str(country_dict[c])
+
+with open_database('GeoLite2-City.mmdb') as db:
+	for a in csv_data:
+		if a["REGION"]:
+			r = a["REGION"]
+
+			if r in region_dict:
+				region_dict[cc] +=1
+			else:
+				region_dict[cc] = 1
+		else:
+			c = a["CONFIRM_IP"]
+			match = geolite2.lookup(c)
+			if match:
+				a["REGION"] = match.subdivisions
+				if match.subdivisions in region_dict:
+					region_dict[match.country] +=1
+				else:
+					region_dict[match.country] =1
+			else:
+				unknown_data.append(a)
+
+
 
 # for u in unknown_data:
 # 	print u["Email Address"] + ": " + u["CONFIRM_IP"]
